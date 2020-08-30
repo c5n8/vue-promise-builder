bba318f6a3c4269852d60ce329ea8f77979ae867
Chabib Nurochman <chabib.nr@gmail.com>
2020-08-30 18:44:46 +0700

    Init sfc-rollup

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { VuePromiseBuilderSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuePromiseBuilderSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-promise-builder-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VuePromiseBuilderSample } from './vue-promise-builder-sample.vue';
